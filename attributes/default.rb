8a589266f4340e9424367b430ed8209c2b453c04
andy <andy@cheft.ci.snops.net>
2013-10-24 14:16:01 -0400

    Chef Training cookbook - initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['apache']['sites']['clowns'] = { "port" => 80, "image" => "http://25.media.tumblr.com/tumblr_m08mwvUUrj1qbppbno1_r1_500.jpg" }
+default['apache']['sites']['bears'] = { "port" => 81, "image" => "http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2012/11/14/1352905427316/Polar-Bear-009.jpg" }
+default['apache']['sites']['ponies'] = { "port" => 83, "image" => "http://i.telegraph.co.uk/multimedia/archive/02459/pony620_2459882b.jpg" }
